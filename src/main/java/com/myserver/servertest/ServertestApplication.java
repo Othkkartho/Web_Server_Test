c5e9e9af4df5e6c1b8f7a0b386464c264b0887ea
qhtkdrma <kimwonkyu15@gmail.com>
2022-03-13 20:10:40 +0900

    Main Code

---
+++ src/main/java/com/myserver/servertest/ServertestApplication.java
@@ -0,0 +1,26 @@
+package com.myserver.servertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ServertestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ServertestApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
