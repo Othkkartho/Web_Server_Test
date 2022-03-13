0f6495e775c2c797d6fe838a7192f38baf3c6d0a
qhtkdrma <kimwonkyu15@gmail.com>
2022-03-13 20:11:57 +0900

    Test Code

---
+++ src/test/java/com/myserver/servertest/Jpa/UserJpaRepoTest.java
@@ -0,0 +1,56 @@
+package com.myserver.servertest.Jpa;
+
+import com.myserver.servertest.advice.exception.CUserNotFoundException;
+import com.myserver.servertest.domain.user.User;
+import com.myserver.servertest.domain.user.UserJpaRepo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserJpaRepoTest {
+
+    @Autowired
+    private UserJpaRepo userJpaRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private String name = "woonsik";
+    private String email = "dnstlr2933@naver.com";
+    private String password = "myPassWord";
+
+
+    @Test
+    public void 회원저장_후_이메일로_회원검색() throws Exception {
+
+        //given
+        userJpaRepo.save(User.builder()
+                .name(name)
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .nickName(name)
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build());
+
+        //when
+        User user = userJpaRepo.findByEmail(email).orElseThrow(CUserNotFoundException::new);
+
+        //then
+        assertNotNull(user);
+        assertEquals(user.getUsername(), user.getUsername());
+        assertThat(user.getName()).isEqualTo(name);
+        assertThat(user.getNickName()).isEqualTo(name);
+    }
+}
